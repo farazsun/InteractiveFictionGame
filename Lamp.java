a31349f861af2f8df76e8556d1286765189849c8
farazsun <114952845+farazsun@users.noreply.github.com>
2023-04-04 17:52:04 -0400

    Update and rename Lamp (1).java to Lamp.java

---
+++ Lamp.java
@@ -0,0 +1,19 @@
+/*
+* Lamp.java
+* Author: Faraz Sunil
+*/
+public class Lamp {
+
+	// Boolean variable indicating if lamp is lit.
+	private boolean isLit;
+	
+	// This method returns isLit and thus if the lamp is lit.
+	public boolean getIsLit() {
+		return isLit;
+	}
+	
+	// This method sets isLit to the parameter.
+	public void setIsLit(boolean isLit) {
+		this.isLit = isLit;
+	}
+}
