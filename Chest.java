383273733764ef6c4fa5947fc05286bc9801ddc3
farazsun <114952845+farazsun@users.noreply.github.com>
2023-04-04 17:53:55 -0400

    Update and rename Chest (1).java to Chest.java

--- Chest.java
+++ Chest.java
@@ -1,18 +1,6 @@
 /*
-* [Chest].java
-* Author: [RYAN CULLEN]
-* Statement of Academic Honesty:
-*
-* The following code represents my own work. I have neither
-* received nor given inappropriate assistance. I have not copied
-* or modified code from anywhere other than the authorized
-* sources. I recognize that any unauthorized sharing, assistance,
-* or plagiarism will be handled in accordance with both the
-* University of Georgia's Academic Honesty Policy and the
-* policies of this course. I recognize that my work is based on
-* an assignment created by the Department of Computer
-* Science at the University of Georgia. Any publishing or posting
-* of source code at any time for this project is prohibited.
+* Chest.java
+* Author: Faraz Sunil
 */
 public class Chest {
 	
